06c4ca3109d205d7a665b0f70b7f6f97068e12b4
bHeau <49450040+beauhodes@users.noreply.github.com>
2022-02-23 10:00:10 -0600

    template

---
+++ test/Lottery.t.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "ds-test/test.sol";
+import 'src/Lottery.sol';
+
+contract LotteryTest is DSTest {
+    Lottery lotInst;
+
+    address currencyAddr;
+    address coordinatorAddr;
+    address linkAddr;
+    uint256 keyHash;
+
+    function setUp() public {
+        lotInst = new Lottery(currencyAddr, coordinatorAddr, linkAddr, keyHash);
+    }
+
+    function test1(string memory _greeting) public {
+        //todo
+    }
+}
